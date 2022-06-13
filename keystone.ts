8e444ea34ee1140944da5f0a2c404914412b28de
Thomas Lekanger <thomas.lekanger@hotmail.com>
2022-06-13 11:47:43 +0200

    chore: add auth to example

--- keystone.ts
+++ keystone.ts
@@ -1,17 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { config } from "@keystone-6/core"
 
 import { lists } from "./schema"
 
+import { session, withAuth } from "./auth"
+
 const url = process.env.DATABASE_URL || "file:./keystone.db"
 const port = parseInt(process.env.PORT || "") || 3002
-
-export default config({
-  db: {
-    provider: "sqlite",
-    url,
-  },
-  server: {
-    port,
-  },
-  lists,
-})
+export default withAuth(
+  config({
+    db: {
+      provider: "sqlite",
+      url,
+    },
+    ui: {
+      isAccessAllowed: context => !!context.session?.data,
+    },
+    server: {
+      port,
+    },
+    lists,
+    session,
+  }),
+)
